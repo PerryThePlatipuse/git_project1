3c03eab534269a219c14b391ea2f1b64b88c7911
Alex Shcherbakov <shcherbakov.032005@gmail.com>
2021-11-23 12:23:32 +0300

    Super_Puper_Client_for_dydx-python

---
+++ main.py
@@ -0,0 +1,12 @@
+from dydx.client import Client
+import dydx.constants as consts
+import dydx.util as utils
+from pprint import pprint
+
+# create a new client with a private key (string or bytearray)
+client = Client(
+    private_key='0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d'
+)
+
+pairs = client.get_pairs()
+pprint(pairs)
